35cffdb9818f62310d62c2b5c69bd8e1042167de
Joe Rowley <joe.rowley@bodybuilding.com>
2013-02-10 17:44:55 -0700

    Initial project creation for Android, mock up of Dashboard

---
+++ LuckyPawsAndroid/src/com/luckypawsdaycare/activities/MainScreen.java
@@ -0,0 +1,20 @@
+package com.luckypawsdaycare.activities;
+
+/*
+ * Copyright (c) 2013 Lucky Paws, Inc
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
