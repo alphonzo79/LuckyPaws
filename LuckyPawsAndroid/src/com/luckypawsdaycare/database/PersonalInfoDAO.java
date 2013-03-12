22e82baec0ab0b99c19c0884a1a6da0d0ae8b444
Joe Rowley <joe.rowley@bodybuilding.com>
2013-03-11 22:20:40 -0600

    Edit Personal Info layout and getPersonalInfo and setPersonalInfo in PersonalInfoDAO

--- LuckyPawsAndroid/src/com/luckypawsdaycare/database/PersonalInfoDAO.java
+++ LuckyPawsAndroid/src/com/luckypawsdaycare/database/PersonalInfoDAO.java
@@ -8,6 +8,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteStatement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PersonalInfoDAO extends DatabaseHelper {
     Context context;
@@ -34,6 +40,68 @@ public class PersonalInfoDAO extends DatabaseHelper {
             result = (count == 1);
         } catch(SQLiteException e) {
             e.printStackTrace();
+        } finally {
+            rs.close();
+            db.close();
+        }
+
+        return result;
+    }
+
+    public Map<String, String> getPersonalInfo() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String stmt = "SELECT firstName, lastName, eMail, phone1, phone2, phone3, addressStreet, addressCity, addressState, " +
+                "addressZip FROM personalInfo WHERE _id = 1;";
+
+        Cursor rs = db.rawQuery(stmt, null);
+        Map<String, String> result = new HashMap<String, String>(10);
+        try{
+            rs.moveToFirst();
+            for(String column : rs.getColumnNames()) {
+                int index = rs.getColumnIndex(column);
+                result.put(column, rs.getString(index));
+            }
+        } catch(SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            rs.close();
+            db.close();
+        }
+        return result;
+    }
+
+    public boolean setPersonalInfo(Map<String, String> updateValues) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        //Build up the query. We will also use an ArrayList to hold the keys so we can guarantee to bind the args in the right order
+        List<String> keys = new ArrayList<String>();
+        keys.addAll(updateValues.keySet());
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i < keys.size(); i++) {
+            sb.append(keys.get(i) + " = '?', ");
+        }
+        String updateArgs = sb.toString();
+        //Remove the last comma
+        updateArgs = updateArgs.substring(0, updateArgs.length() - 2);
+
+        SQLiteStatement stmt = db.compileStatement("UPDATE personalInfo set " + updateArgs + "WHERE _id = 1;");
+        for(int i = 0; i < keys.size(); i++) {
+            stmt.bindString(i + 1, keys.get(i));
+        }
+
+        boolean result = false;
+        db.beginTransaction();
+        try {
+            stmt.execute();
+            result = true;
+            db.setTransactionSuccessful();
+        } catch(SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+            stmt.clearBindings();
+            stmt.close();
         }
 
         return result;
